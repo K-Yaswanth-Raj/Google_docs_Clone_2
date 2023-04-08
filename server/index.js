f1b8583139442296c52c7ded0e2b5d7f5d259e27
Yash <kandepu.yashwanth30@gmail.com>
2023-04-08 22:24:26 +0530

    new change

--- server/index.js
+++ server/index.js
@@ -6,5 +6,5 @@ const PORT = process.env.PORT | 3001;
 const app = express();
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log('Connected at port 30001');
+    console.log('Connected at port 3001');
 });
