b1b984d47ce75c3c696096f6b0e63692f921e243
Yash <kandepu.yashwanth30@gmail.com>
2023-04-09 15:45:38 +0530

    added database

--- server/index.js
+++ server/index.js
@@ -5,6 +5,16 @@ const PORT = process.env.PORT | 3001;
 
 const app = express();
 
+const DB = 'mongodb+srv://yash:yash123@cluster0.nk12ari.mongodb.net/?retryWrites=true&w=majority';
+
+
+
+mongoose.connect(DB).then(() => {
+    console.log('Connection Successful');
+}).catch((e) => {
+    console.log('Error')
+});
+
 app.listen(PORT, '0.0.0.0', () => {
     console.log('Connected at port 3001');
 });
---
+++ server/routes/user.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const authRouter = express.Router.Router();
+
+authRouter.post('/api/signup', async (req,res) => {
+    try{
+        const {name,email} = req.body;
+    } catech (e) {
+
+    }
+});
+
+authRouter.get('/api/get',(req,res) => {
+    
+});
