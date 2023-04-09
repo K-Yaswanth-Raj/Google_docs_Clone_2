78e246b63723ac283a90831d67dba740aec7a135
Yash <kandepu.yashwanth30@gmail.com>
2023-04-09 21:27:54 +0530

    client side api 1:45:59

---
+++ lib/model/user_model.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class UserModel {
+  final String email;
+  final String name;
+  final String uid;
+  final String token;
+
+  UserModel({required this.email, required this.name, required this.uid, required this.token});
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'email': email,
+      'name': name,
+      'uid': uid,
+      'token': token,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      email: map['email'] as String,
+      name: map['name'] as String,
+      uid: map['uid'] as String,
+      token: map['token'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) => UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  UserModel copyWith({
+    String? email,
+    String? name,
+    String? uid,
+    String? token,
+  }) {
+    return UserModel(
+      email: email ?? this.email,
+      name: name ?? this.name,
+      uid: uid ?? this.uid,
+      token: token ?? this.token,
+    );
+  }
+}
--- lib/repository/auth_repo.dart
+++ lib/repository/auth_repo.dart
@@ -1,25 +1,42 @@
+import 'dart:convert';
+
+import 'package:google_docs_clone/constants.dart';
+import 'package:http/http.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_docs_clone/model/user_model.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 final authRepoProvider = Provider<AuthRepo>((ref) {
-  return AuthRepo(
-    googleSignIn: GoogleSignIn(),
-  );
+  return AuthRepo(googleSignIn: GoogleSignIn(), client: Client());
 });
 
 class AuthRepo {
   final GoogleSignIn _googleSignIn;
-
-  AuthRepo({
-    required GoogleSignIn googleSignIn,
-  }) : _googleSignIn = googleSignIn;
+  final Client _client;
+  AuthRepo({required GoogleSignIn googleSignIn, required Client client})
+      : _googleSignIn = googleSignIn,
+        _client = client;
 
   void signInWithGoogle() async {
     try {
       final user = await _googleSignIn.signIn();
       if (user != null) {
-        print(user.email);
-        print(user.displayName);
+        final userAcc = UserModel(
+            email: user.email, name: user.displayName!, uid: '', token: '');
+        var res = await _client.post(Uri.parse('$host/api/signup'),
+            body: userAcc.toJson(),
+            headers: {
+              'Content-Type': 'application/json; charset=UTF-8',
+            });
+
+        switch (res.statusCode) {
+          case 200:
+            final newUser = userAcc.copyWith(
+              uid: jsonDecode(res.body)['user']['_id'],
+            );
+            break;
+          
+        }
       }
     } catch (e) {
       print(e);
