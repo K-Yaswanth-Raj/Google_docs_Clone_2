6d8dd5b5f1de07dc03ed9dcc78463e533e8b5cd1
Yash <kandepu.yashwanth30@gmail.com>
2023-04-15 16:12:23 +0530

    2:42:09

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_docs_clone/model/error_model.dart';
+import 'package:google_docs_clone/repository/auth_repo.dart';
+import 'package:google_docs_clone/screens/home_screen.dart';
 import 'package:google_docs_clone/screens/login_screen.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
+
+  @override
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  ErrorModel? errorModel;
+  @override
+  void initState() {
+    super.initState();
+    getUserData();
+  }
+
+  void getUserData() async {
+    errorModel = await ref.read(authRepoProvider).getUserData();
+    if (errorModel != null && errorModel!.data != null) {
+      ref.read(useProvider.notifier).update((state) => errorModel!.data);
+    }
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final user = ref.watch(useProvider);
     return MaterialApp(
       theme: ThemeData(useMaterial3: true),
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: user == null ? LoginScreen() : HomeScreen(),
     );
   }
 }
--- lib/repository/auth_repo.dart
+++ lib/repository/auth_repo.dart
@@ -2,13 +2,17 @@ import 'dart:convert';
 
 import 'package:google_docs_clone/constants.dart';
 import 'package:google_docs_clone/model/error_model.dart';
+import 'package:google_docs_clone/repository/local_storage_repo.dart';
 import 'package:http/http.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_docs_clone/model/user_model.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 final authRepoProvider = Provider<AuthRepo>((ref) {
-  return AuthRepo(googleSignIn: GoogleSignIn(), client: Client());
+  return AuthRepo(
+      googleSignIn: GoogleSignIn(),
+      client: Client(),
+      localStorageRepository: LocalStorageRepository());
 });
 
 final useProvider = StateProvider<UserModel?>((ref) => null);
@@ -16,9 +20,14 @@ final useProvider = StateProvider<UserModel?>((ref) => null);
 class AuthRepo {
   final GoogleSignIn _googleSignIn;
   final Client _client;
-  AuthRepo({required GoogleSignIn googleSignIn, required Client client})
+  final LocalStorageRepository _localStorageRepository;
+  AuthRepo(
+      {required LocalStorageRepository localStorageRepository,
+      required GoogleSignIn googleSignIn,
+      required Client client})
       : _googleSignIn = googleSignIn,
-        _client = client;
+        _client = client,
+        _localStorageRepository = localStorageRepository;
 
   Future<ErrorModel> signInWithGoogle() async {
     ErrorModel error = ErrorModel(error: 'Some Error has Occured', data: null);
@@ -40,6 +49,7 @@ class AuthRepo {
               token: jsonDecode(res.body)['token'],
             );
             error = ErrorModel(error: null, data: newUser);
+            _localStorageRepository.setToken(newUser.token);
             break;
         }
       }
@@ -48,5 +58,31 @@ class AuthRepo {
     }
     return error;
   }
-  
+
+  Future<ErrorModel> getUserData() async {
+    ErrorModel error = ErrorModel(error: 'Some Error has Occured', data: null);
+    try {
+      String? token = await _localStorageRepository.getToken();
+      if (token != null) {
+        var res = await _client.get(Uri.parse('$host/'), headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': token,
+        });
+        switch (res.statusCode) {
+          case 200:
+            final newUser = UserModel.fromJson(
+              jsonEncode(jsonDecode(res.body)['user']),
+            ).copyWith(
+              token: token,
+            );
+            error = ErrorModel(error: null, data: newUser);
+            _localStorageRepository.setToken(newUser.token);
+            break;
+        }
+      }
+    } catch (e) {
+      error = ErrorModel(error: e.toString(), data: null);
+    }
+    return error;
+  }
 }
