58f0dc8fc9fd211551cb717d16ec9fd6d3cb9f6c
Yash <kandepu.yashwanth30@gmail.com>
2023-04-16 13:44:56 +0530

    3:08:57

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRouter = require('./routes/auth');
 const cors = require('cors');
+const documentRouter = require('./routes/document');
 
 const PORT = process.env.PORT | 3001;
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(authRouter);
+app.use(documentRouter);
 
 
 const DB = 'mongodb+srv://yash:yash123@cluster0.nk12ari.mongodb.net/?retryWrites=true&w=majority';
---
+++ server/models/document.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const documentSchema = mongoose.Schema({
+    uid:{
+        required: true,
+        type: String,
+    },
+    createdAt: {
+        require: true,
+        type: Number,
+    },
+    title:{
+        require: true,
+        type: String,
+        trim: true
+    },
+    content:{
+        type: Array,
+        default: [],
+    }
+
+});
+
+const Document = mongoose.model('Document',documentSchema);
+
+
+
+module.exports = Document;
